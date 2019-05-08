05d2bea1d636c26434b1d8cd357c70f3999131ec
nisuikiba <tanaka.sae832@mail.kyutech.jp>
2019-05-09 02:38:36 +0900

    add new file

--- static/rotate.js
+++ static/rotate.js
@@ -11,7 +11,8 @@ function init() {
         "x" : new THREE.Vector3(1, 0, 0).normalize(),
         "y" : new THREE.Vector3(0, 1, 0).normalize(),
         "z" : new THREE.Vector3(0, 0, 1).normalize(),
-        "y-z" : new THREE.Vector3(0, 1, 1).normalize()
+        "y-z" : new THREE.Vector3(0, 1, 1).normalize(),
+        "y+z" : new THREE.Vector3(0, 1, -1).normalize()
     };
 
     //レンダラーを作成
@@ -31,8 +32,12 @@ function init() {
     controls.enableDamping = true;
     controls.dampingFactor = 0.2;
 
+    //制御ビットのグループ
     const control_group = new THREE.Group();
     scene.add(control_group);
+
+    //制御ビットの重ね合わせを表現するグループ
+    const super_group = new THREE.Group();
 
     //ベクトル描画
     //標的ビットの軸
@@ -72,6 +77,34 @@ function init() {
     const control_z_arrow = new THREE.ArrowHelper(control_direction_z.normalize(), control_from, length_xyz, 0x9ceb43, 14, 14);
     control_group.add(control_z_arrow);
 
+    //制御ビットの重ね合わせを表すブロッホ級の軸
+    /*
+    const super_from = new THREE.Vector3(0, 240, 0);
+
+    const super_to_x = new THREE.Vector3(0, 240, 120);
+    const super_direction_x = super_to_x.clone().sub(super_from);
+    const super_x_arrow = new THREE.ArrowHelper(super_direction_x.normalize(), super_from, length_xyz, 0x29b0da, 14, 14);
+    super_group.add(super_x_arrow);
+
+    const super_to_y = new THREE.Vector3(120, 240, 0);
+    const super_direction_y = super_to_y.clone().sub(super_from);
+    const super_y_arrow = new THREE.ArrowHelper(super_direction_y.normalize(), super_from, length_xyz, 0xda2932, 14, 14);
+    super_group.add(super_y_arrow);
+
+    const super_to_z = new THREE.Vector3(0, 360, 0);
+    const super_direction_z = super_to_z.clone().sub(super_from);
+    const super_z_arrow = new THREE.ArrowHelper(super_direction_z.normalize(), super_from, length_xyz, 0x9ceb43, 14, 14);
+    super_group.add(super_z_arrow);*/
+
+    const super_x_arrow = new THREE.ArrowHelper(control_direction_x.normalize(), control_from, length_xyz, 0x29b0da, 14, 14);
+    super_group.add(super_x_arrow);
+
+    const super_y_arrow = new THREE.ArrowHelper(control_direction_y.normalize(), control_from, length_xyz, 0xda2932, 14, 14);
+    super_group.add(super_y_arrow);
+
+    const super_z_arrow = new THREE.ArrowHelper(control_direction_z.normalize(), control_from, length_xyz, 0x9ceb43, 14, 14);
+    super_group.add(super_z_arrow);
+
     //制御ビットの状態ベクトル
     const to = new THREE.Vector3(0, 360, 0);
     const direction = to.clone().sub(control_from);
@@ -79,34 +112,47 @@ function init() {
     const arrowHelper = new THREE.ArrowHelper(direction.normalize(), control_from, length, 0xDF013A, 15, 10);
     control_group.add(arrowHelper);
 
-    //制御ビットの球を作成
-    const control_geometry = new THREE.SphereGeometry(120, 30, 30);
-    const control_material = new THREE.MeshLambertMaterial({color: 0x555555, transparent: true, opacity: 0.4});
-    const control_sphere = new THREE.Mesh(control_geometry, control_material);
-    control_sphere.position.set(0, 240, 0);
-    control_group.add(control_sphere);
+    //制御ビットの重ね合わせを表すブロッホ級の状態ベクトル
+    const super_to = new THREE.Vector3(0, 120, 0);
+    const super_direction = super_to.clone().sub(control_from);
+    const super_arrowHelper = new THREE.ArrowHelper(super_direction.normalize(), control_from, length, 0xaa00cc, 15, 10);
+    super_group.add(super_arrowHelper);
 
     //標的ビットの球を作成
-    const target_geometry = new THREE.SphereGeometry(120, 30, 30);
+    const sphere_geometry = new THREE.SphereGeometry(120, 30, 30);
     const target_material = new THREE.MeshLambertMaterial({color: 0xffffff, transparent: true, opacity: 0.4});
-    const target_sphere = new THREE.Mesh(target_geometry, target_material);
+    const target_sphere = new THREE.Mesh(sphere_geometry, target_material);
     target_sphere.position.set(0, 0, 0);
     scene.add(target_sphere);
 
+    //制御ビットの球を作成
+    const control_material = new THREE.MeshLambertMaterial({color: 0x000000, transparent: true, opacity: 0.2});
+    const control_sphere = new THREE.Mesh(sphere_geometry, control_material);
+    control_sphere.position.set(0, 240, 0);
+    control_group.add(control_sphere);
+
+    //制御ビットの重ね合わせを表すブロッホ級の球を作成
+    const super_sphere = new THREE.Mesh(sphere_geometry, control_material);
+    super_sphere.position.set(0, 240, 0);
+    super_group.add(super_sphere);
+
     //ワイヤフレーム描画
     const control_wire_geometry = new THREE.EdgesGeometry(control_sphere.geometry);
-    const control_wire_material = new THREE.LineBasicMaterial({color: 0xBDBDBD,linewidth:2});
-    const control_wireframe = new THREE.LineSegments(control_wire_geometry, control_wire_material);
+    const wire_material = new THREE.LineBasicMaterial({color: 0xBDBDBD,linewidth:2});
+    const control_wireframe = new THREE.LineSegments(control_wire_geometry, wire_material);
     control_sphere.add(control_wireframe);
 
     const target_wire_geometry = new THREE.EdgesGeometry(target_sphere.geometry);
-    const target_wire_material = new THREE.LineBasicMaterial({color: 0xBDBDBD,linewidth:2});
-    const target_wireframe = new THREE.LineSegments(target_wire_geometry, target_wire_material);
+    const target_wireframe = new THREE.LineSegments(target_wire_geometry, wire_material);
     target_sphere.add(target_wireframe);
+
+    const super_wire_geometry = new THREE.EdgesGeometry(super_sphere.geometry);
+    const super_wireframe = new THREE.LineSegments(super_wire_geometry, wire_material);
+    super_sphere.add(super_wireframe);
     
     // 平行光源
-    const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-    directionalLight.position.set(20, 10, 30);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
+    directionalLight.position.set(30, 30, 30);
     scene.add(directionalLight);
     
     //レンダリング
@@ -122,6 +168,7 @@ function init() {
     var target = new THREE.Quaternion();
     var control_state = "zero";
     
+    /*
     //xゲート
     document.getElementById('x').onclick = function xgate(){
        if(flag < Math.PI){
@@ -138,20 +185,6 @@ function init() {
                control_state = "zero";
            }
        }
-    }
-
-    /*
-    //zゲート
-    document.getElementById('z').onclick = function zgate(){
-        if(flag < Math.PI){
-            target.setFromAxisAngle(Axis["y"], Math.PI/50);
-            target.multiply(quaternion.clone());  
-            quaternion.copy(target);  
-            flag += Math.PI/50;
-            requestAnimationFrame(zgate);
-        }else{
-            flag = 0;
-        }
     }*/
 
     //hゲート
@@ -164,155 +197,78 @@ function init() {
             requestAnimationFrame(hgate);
         }else{
             flag = 0;
-            if(control_state == "zero"){
-                control_state = "bell_plus";
-            }else if(control_state == "one"){
-                control_state = "bell_minus";
-            }else if(control_state == "bell_plus"){
-                control_state = "zero";
-            }else if(control_state == "bell_minus"){
-                control_state = "one";
-            }
+        }
+        superposition();
+        if(control_state == "zero"){
+            control_state = "bell_plus";
+        }else if(control_state == "one"){
+            control_state = "bell_minus";
+        }else if(control_state == "bell_plus"){
+            control_state = "zero";
+        }else if(control_state == "bell_minus"){
+            control_state = "one";
         }
     }
-
-    /*
-    //sゲート
-    document.getElementById('s').onclick = function sgate(){
-        if(flag < Math.PI/2){
-            target.setFromAxisAngle(Axis["y"], Math.PI/50);
-            target.multiply(quaternion.clone());  
-            quaternion.copy(target);  
-            flag += Math.PI/50;
-            requestAnimationFrame(sgate);
-        }else{
-            flag = 0;
-        }
-    }
-
-    //tゲート
-    document.getElementById('t').onclick = function tgate(){
-        if(flag < Math.PI/4){
-            target.setFromAxisAngle(Axis["y"], Math.PI/48);
-            target.multiply(quaternion.clone());  
-            quaternion.copy(target);  
-            flag += Math.PI/48;
-            requestAnimationFrame(tgate);
-        }else{
-            flag = 0;
-        }
-    }*/
 
     //はじめからボタン
     document.getElementById('start').onclick = function start(){
         location.reload();
     }
 
-    var sphere_quaternion = control_group.quaternion;
-    var sphere_target = new THREE.Quaternion();
-
-    /*
-    function entangle(){
-        const blue_to = new THREE.Vector3(0, 360, 0);
-        const red_to = new THREE.Vector3(0, 120, 0);
-        const blue_direction = blue_to.clone().sub(control_from);
-        const red_direction = red_to.clone().sub(control_from);
-        const blue_arrow = new THREE.ArrowHelper(blue_direction.normalize(), control_from, length, 0x29b0da, 15, 10);
-        const red_arrow = new THREE.ArrowHelper(red_direction.normalize(), control_from, length, 0xda2932, 15, 10);
-        control_group.add(blue_arrow);
-        control_group.add(red_arrow);
+    function superposition(){
+        scene.add(super_group);
+        const purple_to = new THREE.Vector3(0, 360, 0);
+        const purple_direction = purple_to.clone().sub(control_from);
+        const purple_arrow = new THREE.ArrowHelper(purple_direction.normalize(), control_from, length, 0xaa00cc, 15, 10);
+        control_group.add(purple_arrow);
     }
 
-    function dispose_entangle(){
-        control_group.remove(blue_arrow);
-        control_group.remove(red_arrow);
-    }*/
+    var control_quaternion = control_group.quaternion;
+    var control_target = new THREE.Quaternion();
+
+    var super_quaternion = super_group.quaternion;
+    var super_target = new THREE.Quaternion();
+
+    function dispose_bec(){
+        control_group.remove(arrowHelper);
+    }
 
     //double_cx
     document.getElementById('cx').onclick = function double_cx(){
         if(control_state == "one"){
             //cxゲート
+            /*
             (function cx(){
                 if(flag < Math.PI){
-                    sphere_target.setFromAxisAngle(Axis["z"], Math.PI/50);
-                    sphere_target.multiply(sphere_quaternion.clone());  
-                    sphere_quaternion.copy(sphere_target);  
+                    control_target.setFromAxisAngle(Axis["z"], Math.PI/50);
+                    control_target.multiply(control_quaternion.clone());  
+                    control_quaternion.copy(control_target);  
                     flag += Math.PI/50;
                     requestAnimationFrame(cx);
                 }else{
                     flag = 0;
                 }
-            })();
+            })();*/
         }else if((control_state == "bell_plus")||(control_state == "bell_minus")){
             //chゲート
             (function ch(){
+                dispose_bec()
                 if(flag < Math.PI){
-                    sphere_target.setFromAxisAngle(Axis["y-z"], Math.PI/50);
-                    sphere_target.multiply(sphere_quaternion.clone());  
-                    sphere_quaternion.copy(sphere_target);  
+                    control_target.setFromAxisAngle(Axis["y-z"], Math.PI/50);
+                    control_target.multiply(control_quaternion.clone());  
+                    control_quaternion.copy(control_target);  
+                    super_target.setFromAxisAngle(Axis["y+z"], Math.PI/50);
+                    super_target.multiply(super_quaternion.clone());  
+                    super_quaternion.copy(super_target);  
                     flag += Math.PI/50;
                     requestAnimationFrame(ch);
                 }else{
                     flag = 0;
-                    //entangle();
                 }
             })();
         }else{
             alert("The State is Zero");
         }
     }
-
-    /*
-    //czゲート
-    document.getElementById('cz').onclick = function cz(){
-        if(flag < Math.PI){
-            sphere_target.setFromAxisAngle(Axis["y"], Math.PI/50);
-            sphere_target.multiply(sphere_quaternion.clone());  
-            sphere_quaternion.copy(sphere_target);  
-            flag += Math.PI/50;
-            requestAnimationFrame(cz);
-        }else{
-            flag = 0;
-        }
-    }
-
-    //chゲート
-    document.getElementById('ch').onclick = function ch(){
-        if(flag < Math.PI){
-            sphere_target.setFromAxisAngle(Axis["y-z"], Math.PI/50);
-            sphere_target.multiply(sphere_quaternion.clone());  
-            sphere_quaternion.copy(sphere_target);  
-            flag += Math.PI/50;
-            requestAnimationFrame(ch);
-        }else{
-            flag = 0;
-        }
-    }
-
-    //csゲート
-    document.getElementById('cs').onclick = function cs(){
-        if(flag < Math.PI/2){
-            sphere_target.setFromAxisAngle(Axis["y"], Math.PI/50);
-            sphere_target.multiply(sphere_quaternion.clone());  
-            sphere_quaternion.copy(sphere_target);  
-            flag += Math.PI/50;
-            requestAnimationFrame(cs);
-        }else{
-            flag = 0;
-        }
-    }
-
-    //ctゲート
-    document.getElementById('ct').onclick = function ct(){
-        if(flag < Math.PI/4){
-            sphere_target.setFromAxisAngle(Axis["y"], Math.PI/50);
-            sphere_target.multiply(sphere_quaternion.clone());  
-            sphere_quaternion.copy(sphere_target);  
-            flag += Math.PI/50;
-            requestAnimationFrame(ct);
-        }else{
-            flag = 0;
-        }
-    }*/
     
 }
